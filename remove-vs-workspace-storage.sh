ee570bed7fae1ccdf761d4c3554b55e86ca609d7
gretmn102 <erlgreywatson@gmail.com>
2025-01-25 20:43:31 +0200

    feat(cli): add `--find` argument

--- remove-vs-workspace-storage.sh
+++ remove-vs-workspace-storage.sh
@@ -8,34 +8,44 @@ usage() {
   echo
   echo "Options:"
   echo "  -h, --help  Show this help."
+  echo "  --find      Just find the project without remove."
   exit 1
 }
 
-if [[ "$1" == "--help" || "$1" == "-h" ]]; then
-  usage
-  exit 0
-fi
-
-if [ -z "$1" ]; then
-  usage
-  exit 1
-fi
-
-project_pattern=$1
-
-cd ~/AppData/Roaming/VSCodium/User/workspaceStorage
-folders=$(find . -name 'workspace.json' -exec grep -H $project_pattern {} + | awk -F'/' '{print $2}')
-for folder in $folders; do
-  if [ -d "$folder" ]; then
-    full_folder=$(pwd)/$folder
-    read -p "Are you sure you want to delete the folder '$full_folder'? (y/n): " confirm
-    if [[ "$confirm" == "y" || "$confirm" == "Y" ]]; then
-      echo "Deleting the folder '$full_folder'"
-      rm -rf "$full_folder"
+case "$1" in
+  "")
+    usage
+    ;;
+  -h|--help)
+    usage
+    ;;
+  --find)
+    if [[ $# -eq 2 ]]; then
+      project_pattern=$2
+      cd ~/AppData/Roaming/VSCodium/User/workspaceStorage
+      find . -name 'workspace.json' -exec grep -H $project_pattern {} +
     else
-      echo "Skip deleting the folder '$full_folder'"
+      echo "Error: Missing pattern after --find"
+      usage
     fi
-  else
-    echo "Folder '$full_folder' not found!"
-  fi
-done;
+    ;;
+  *)
+    project_pattern=$1
+    cd ~/AppData/Roaming/VSCodium/User/workspaceStorage
+    folders=$(find . -name 'workspace.json' -exec grep -H $project_pattern {} + | awk -F'/' '{print $2}')
+    for folder in $folders; do
+      if [ -d "$folder" ]; then
+        full_folder=$(pwd)/$folder
+        read -p "Are you sure you want to delete the folder '$full_folder'? (y/n): " confirm
+        if [[ "$confirm" == "y" || "$confirm" == "Y" ]]; then
+          echo "Deleting the folder '$full_folder'"
+          rm -rf "$full_folder"
+        else
+          echo "Skip deleting the folder '$full_folder'"
+        fi
+      else
+        echo "Folder '$full_folder' not found!"
+      fi
+    done;
+    ;;
+esac
